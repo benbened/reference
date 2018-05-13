21a5372afb47c7c72da3c266c18f3177dbcf557e
AMATERASU\BBE <services@benedict.es>
2018-05-13 21:57:59 +0200

    updated install.bat with comments

---
+++ install.bat
@@ -0,0 +1,10 @@
+::installation of jekyll
+gem install jekyll bundler
+::installation of grunt
+npm install -g grunt-cli
+::installation of SASS
+npm install -g sass
+::installation of http-server
+npm install http-server -g
+::install npm-dependencies
+npm install
